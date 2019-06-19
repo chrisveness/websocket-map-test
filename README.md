bd9d6cec993ae61b83585e47c519378fae00fc49
Chris Veness <chrisv@movable-type.co.uk>
2019-06-19 13:13:19 +0100

    Update to broadcast to all clients, simulate monitoring of multiple trackers

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 WebSocket Map Test
 ==================
 
-Test of using WebSockets to update marker position on Google Map.
+Test of using WebSockets to update marker positions on Google Map.
 
-This test pushes a JSON message with a random new location for the marker, at random intervals.
+This test broadcasts a JSON messages with an updated location for the marker, at regular intervals.
 
 `npm start` and open localhost:3000/page to see it in action (you will probably have to supply your own Google Maps key).
+
+
+
+
+
+
+Update to broadcast to all clients, simulate monitoring of multiple trackers
--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
 
-import http               from 'http';
-import WebSocket          from 'ws';
-import { promises as fs } from 'fs';
+import http               from 'http'; // nodejs.org/api/http.html
+import WebSocket          from 'ws';   // ws: a Node.js WebSocket library
+import { promises as fs } from 'fs';   // nodejs.org/api/fs.html
 
 
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
+/* Web server  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
 
-const server = http.createServer(); // web server
+const server = http.createServer();
 
 server.on('request', async(req, res) => {
     switch (req.url) {
@@ -23,46 +23,60 @@ server.on('request', async(req, res) => {
 
 async function servePage(res) {
     const file = await fs.readFile('page.html');
-    const html = file.toString('utf8').replace('{{timestamp}}', new Date().toISOString().split('T')[1]);
+    const html = file.toString('utf8');
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(html);
     res.end();
 }
 
-server.listen(3000);
-console.info('Server listening on 3000');
+server.listen(process.env.PORT||3000);
+console.info(`Server listening on port ${process.env.PORT||3000}`);
 
 
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
+/* WebSocket server  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
 
-const wsServer = new WebSocket.Server({ server: server }); // websocket server
+const wsServer = new WebSocket.Server({ server: server });
 
-wsServer.on('connection', async (ws, req) => {
-    console.log('new connection', ws._socket.remoteAddress);
-    let open = true;
-
-    ws.on('message', message => {
-        console.log(`msg rec’d: ‘${message}’`)
-    });
-
+wsServer.on('connection', ws => {
+    console.info('new connection from', ws._socket.remoteAddress);
     ws.on('close', () => {
-        console.log('connection closed');
-        open = false;
+        console.info(ws._socket.remoteAddress, 'left');
     });
-
-    while (open) { // send new location at random intervals
-        const msg = {
-            lat:   Math.random() * 6 + 50,
-            lng:   Math.random() * 8 - 6,
-            title: new Date().toISOString().split('T')[1],
-        };
-        console.log('send msg', msg.title);
-        ws.send(JSON.stringify(msg));
-        await sleep(Math.floor(Math.random()*4000));
-    }
 });
 
+wsServer.broadcast = function(data) {
+    wsServer.clients.forEach(client => {
+        if (client.readyState == WebSocket.OPEN) {
+            client.send(data);
+        }
+    });
+};
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+const trackers = [ // simulate tracker reports
+    { id: 'a', lat: 52.20, lng: 0.12, ts: '00:00:00Z', speed: Math.random()/100, brng: Math.random()*360 },
+    { id: 'b', lat: 52.20, lng: 0.12, ts: '00:00:00Z', speed: Math.random()/100, brng: Math.random()*360 },
+    { id: 'c', lat: 52.20, lng: 0.12, ts: '00:00:00Z', speed: Math.random()/100, brng: Math.random()*360 },
+];
+
+function updateLocations() { // simulate movement of trackers
+    for (const tracker of trackers) { // update tracker locations
+        if (tracker.lat<50 || tracker.lat>56 || tracker.lng<-4 || tracker.lng>2) {
+            // bounce off bounding box with new random speed/bearing
+            tracker.speed = Math.random()/100;
+            tracker.brng = (tracker.brng + 180 + Math.random()*90-45) % 360;
+        }
+        tracker.lat += tracker.speed * Math.cos(tracker.brng*Math.PI/180);
+        tracker.lng += tracker.speed * Math.sin(tracker.brng*Math.PI/180);
+        tracker.ts = new Date().toISOString().split('T')[1];
+    }
 }
+
+function receiveTrackerReport() { // simulate receipt of tracker report; broadcast update
+    const t = Math.floor(Math.random()*trackers.length); // select random tracker to broadcast
+    wsServer.broadcast(JSON.stringify(trackers[t]));
+}
+
+setInterval(updateLocations, 100);      // update tracker locations every 100 ms
+setInterval(receiveTrackerReport, 200); // 'receive report' from tracker every 200 ms
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
